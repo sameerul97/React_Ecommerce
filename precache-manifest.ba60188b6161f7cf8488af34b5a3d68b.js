3ce5d63913796ca046191135ae57d692681a2ed1
sameerul97 <sameerul97@gmail.com>
2019-02-25 16:23:25 +0000

    Updates

---
+++ precache-manifest.ba60188b6161f7cf8488af34b5a3d68b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cbdd9cd74a67a95ff7a",
+    "url": "/static/js/main.0cbdd9cd.chunk.js"
+  },
+  {
+    "revision": "0a26e5e33b3149cab02e",
+    "url": "/static/js/1.0a26e5e3.chunk.js"
+  },
+  {
+    "revision": "0cbdd9cd74a67a95ff7a",
+    "url": "/static/css/main.142287be.chunk.css"
+  },
+  {
+    "revision": "2aa00ecf6f0cd169364eee45da081b7d",
+    "url": "/index.html"
+  }
+];
